2dcf88fcfd55aad84bca4449ab1b901d8d829092
Fernando Ghiberti <ghiberti85@Fernandos-MacBook-Pro.local>
2023-10-24 22:57:10 -0300

    First commit - Configuring Vite and Create Static Post Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post';
+
+export function App() {
+
+  return (
+    <>
+      <Post
+        author="Fernando Ghiberti"
+        content="Lorem ipsum dolor sit amet consectetur adipiscing elit."
+      />
+
+      <Post
+        author="Gabriel Ghiberti"
+        content="Sou um post muito legal!"
+      />
+    </>
+  )
+}
