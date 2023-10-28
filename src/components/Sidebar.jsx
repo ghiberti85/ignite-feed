6881ea41cc4f84a69db079cdeffc735bda5319f3
ghiberti85 <ghiberti85@gmail.com>
2023-10-28 00:16:42 -0300

    Finishing aside style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,16 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/ghiberti85.png" />
                 <strong>Fernando Ghiberti</strong>
                 <span>Software Engineer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu  perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu  perfil
+                </a>
             </footer>
         </aside>
     );
