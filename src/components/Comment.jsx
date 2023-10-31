3d1745a3f6a9fc3c5d26691462f7ce0823e85137
ghiberti85 <ghiberti85@gmail.com>
2023-10-30 21:58:22 -0300

    Comment Component Style

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,25 +5,29 @@ export function Comment () {
     return (
         <div className={styles.comment}>
             <img src="https://github.com/ghiberti85.png" />
+            
             <div className={styles.commentBox}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Fernando Ghiberti</strong>
-                        <time title="30 de Outubro às 15:28h" dateTime="2023-10-30 15:28:30">Cerca de 1h atrás</time>
-                    </div>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Fernando Ghiberti</strong>
+                            <time title="30 de Outubro às 15:28h" dateTime="2023-10-30 15:28:30">Cerca de 1h atrás</time>
+                        </div>
 
-                    <button title="Deletar comentário">
-                        <Trash  size={20} />
+                        <button title="Deletar comentário">
+                            <Trash  size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom, parabéns!!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
                     </button>
-                </header>
-
-                <p>Muito bom, parabéns!!!</p>
+                </footer>
             </div>
-
-            <footer>
-                <ThumbsUp />
-                Aplaudir <span>20</span>
-            </footer>
         </div>
     );
 }
